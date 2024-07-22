c2377b8d6dee853529d99837fbe83e9c6a83e59d
coconutlad <coconutlad@users.noreply.github.com>
2024-07-22 02:28:02 +0000

    deploy: 1916106e81406c9c3f99afdc8c881e8a3f6a9fd7

--- en.search-data.min.8983296b4e447045c3bdf4707077531cd82fcc54f3c3bc8da299a7da7fcb4dac.js
+++ en.search-data.min.8983296b4e447045c3bdf4707077531cd82fcc54f3c3bc8da299a7da7fcb4dac.js
@@ -234,9 +234,21 @@ Those files are defined in the Groovy language by defualt. You can also have &ld
 Gradle itself runs on the JVM, since it is built using Java.
 Build # Gradle uses a concept of &ldquo;build&rdquo;. A project having a build.gradle file is called a build. Nearly every gradle command runs wrt a &ldquo;build&rdquo;.
 You can create such a buildscript for your project by doing gradle init (which also adds extra stuff e.g., wrapper &amp; settings.gradle files)
-Tasks # They have lifecycles, and we can add code to run at some stages of these lifecyles.
+There is a build lifecycle which happens when gradle xyzTask is run. It does &ldquo;initialization&rdquo;, which is reading the settings file, then does &ldquo;configuration&rdquo;, which is executing the buildscripts for each project involved, creating the task graph etc., then does the actual execution of each task involved.
+This means any code written outside of a task defintion in the buildscript will be executed in the configuration phase.
+Tasks can be chosen to run in parallel with the --parallel flag.
+Tasks # They have lifecycles, and we can add code to run at some stages of these lifecyles, through some lifecycle hooks.
 Then you can invoke the task from gradle e.g., gradle myNewTask, and that will go through full life cycle of the task.
 Run gradle tasks to see all available tasks for the &ldquo;build&rdquo;.
+Some lifecycle hooks which are called during a task&rsquo;s execution lifecyle are doFirst, and doLast.
+Tasks are objects of the Task type, and are instances of the DefaultTask class by default.
+There are other inbuilt Task sub-classes as well e.g., Copy, Jar, Zip etc. of which task objects can be instantiated.
+To know more about a task, run gradle help --tasks taskName.
+It is also possible to define a new task class which takes inputs &amp; produces outputs. Those are of types TaskInputs, and TaskOutputs, and are defined with one of the @Input, or @Output annotations e.g.,
+abstract class TestTask : DefaultTask() { @Input val xyz = &#34;&#34; @OutputFile val abc: File = File(&#34;/var/tmp/hi.txt&#34;) @TaskAction fun someFuncName() { abc.createNewFile() } } To add dependencies between tasks, just set the dependsOn property in the depending task.
+The list of all tasks are available in a TaskCollection, which is by default called tasks. To add to it, we do tasks.register(&quot;newOne&quot;), and to fetch we do tasks.named(&quot;newOne&quot;)
+The @TaskAction annotation defines the function which should be run when an task of that class is invoked.
+That function is called after doFirst, and before doLast. It is also possible to create tasks which don&rsquo;t have any action at all e.g., build. They can have other properties and functions, but don&rsquo;t really have an action to do by themselves.
 Projects # When you run gradle inside a &ldquo;build&rdquo;, it first looks for the settings.gradle file, creates a &ldquo;Settings&rdquo; object, determines which projects are in the &ldquo;build&rdquo;, and then looks for the buildscripts (some *.gradle file) for each of those projects to construct &ldquo;Project&rdquo; objects. There is only one root project for each &ldquo;build&rdquo;.
 Plugins # These add extra tasks to the &ldquo;build&rdquo; and maybe even extra settings. Once you add a plugin, you can re-run gradle tasks to see what new stuff is there. Plugins can be added via plugin { id: 'java' } in the buildscript or apply plugin 'java' (deprecated).
 Syntax # The groovy &amp; kotlin buildscripts have stuff like this:
